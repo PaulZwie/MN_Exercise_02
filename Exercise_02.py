aeb557751eb09e46f2c8a881c23ffb9e054b4d8a
Paul Zwiessele <paul.zwiessele@fau.de>
2025-01-08 11:24:58 +0100

    P2P calculation and plots

--- Exercise_02.py
+++ Exercise_02.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
-import Data.spikeTriggeredAveraging as sta
-
 
 def import_data(file_path):
     return sp.io.loadmat(file_path)
@@ -147,6 +145,27 @@ def plot_muap_shapes(muap_shapes, mu_index):
     plt.show()
 
 
+def calculate_sta(emg, mu, mu_num, time, fsamp):
+    sample_window = int(((time * fsamp) // 2).item())
+    mu = mu[mu_num]
+
+    sta = []
+
+    for y in range(emg.shape[1]):
+        for x in range(emg.shape[0]):
+            sig = emg[x][y]
+
+            electrode_sta = []
+
+            for mu_idx in mu[0]:
+                if sample_window < mu_idx < (sig.shape[1] - sample_window):
+                    electrode_sta.append(sig[0][mu_idx - sample_window:mu_idx + sample_window])
+
+            sta.append(np.mean(electrode_sta, axis=0))
+
+    return sta
+
+
 def plot_sta(sta_sig, mu_num):
     fig, axs = plt.subplots(16, 1, figsize=(10, 20), sharex=True)
     fig.suptitle(f"Spike Triggered Averages of Motor Unit Number: {mu_num}")
@@ -161,36 +180,103 @@ def plot_sta(sta_sig, mu_num):
     plt.show()
 
 
+def calculate_p2p(emg, mu, time, fsamp):
+    sample_window = int(((time * fsamp) // 2).item())
+
+    max_p2p = []
+    for unit in mu:
+        unit_max = []
+        for y in range(emg.shape[1]):
+            for x in range(emg.shape[0]):
+                sig = emg[x][y]
+                electrode_max = 0
+
+                for mu_idx in unit[0]:
+                    if sample_window < mu_idx < (sig.shape[1] - sample_window):
+                        local_window = sig[0][mu_idx - sample_window:mu_idx + sample_window]
+                        minmax = [np.min(local_window), np.max(local_window)]
+                        dist = np.diff(minmax)
+                    else:
+                        dist = electrode_max
+
+                    if dist > electrode_max:
+                        electrode_max = dist
+
+                unit_max.append(electrode_max)
+        max_p2p.append(np.max(unit_max))
+    return max_p2p
+
+
+def test(emg, mu, time, fsamp):
+    dist_all = []
+    for unit in range(len(mu)):
+        sta = calculate_sta(emg, mu, unit, time, fsamp)
+        dist_sta = []
+        for spike in sta:
+            min, max = np.min(spike), np.max(spike)
+            dist = max - min
+            dist_sta.append(dist)
+
+        dist_all.append(np.max(dist_sta))
+
+    return dist_all
+
+
+def plot_max_p2p(p2p):
+    fig, axs = plt.subplots(figsize=(10, 5))
+
+    x_ax = np.arange(0, len(p2p))
+    axs.scatter(x=x_ax, y=p2p)
+
+    # Add linear regression line
+    slope, intercept = np.polyfit(x_ax, p2p, 1)
+    regression_line = slope * x_ax + intercept
+    axs.plot(x_ax, regression_line, color='red', label='Linear Regression')
+
+    axs.set_ylim(0, 4)
+    axs.set_ylabel("Amplitude (mV)")
+    axs.set_xlabel("Motor Unit Recruitment Order ")
+    axs.set_xticks(x_ax)
+    fig.suptitle("Maximum amplitude of the different Motor Unit Action Potentials")
+    axs.grid(True, which='both', linestyle='--', linewidth=0.5)  # Add grid to the plot
+    fig.tight_layout()
+    plt.show()
+
+
 def main():
     cwd = os.getcwd()
     file_path = os.path.join(cwd, "Data", "iEMG_contraction.mat")
 
     data = import_data(file_path)
 
-    emg_signal = data['EMGSig']
+    emg = data['EMGSig']
     fsamp = data['fsamp']
     force_signal = data['force_signal']
     signal_length_samples = data['signal_length_samples']
 
     orig_mu_pulses = data['MUPulses']
-    mu_pulses = pulses_to_list(orig_mu_pulses)
-    sorted_mu = sort_mus(mu_pulses)
+    mu = pulses_to_list(orig_mu_pulses)
+    sorted_mu = sort_mus(mu)
 
     # 1.1
-    firing_matrix = mu_pulses_to_firing_matrix(mu_pulses)
+    firing_matrix = mu_pulses_to_firing_matrix(mu)
 
     # 1.2
     sorted_firing_matrix = mu_pulses_to_firing_matrix(sorted_mu)
 
     # 1.3
-    idr_01 = calculate_idr(mu_pulses[1], fsamp)
-    idr_02 = calculate_idr(mu_pulses[2], fsamp)
+    idr_01 = calculate_idr(mu[1], fsamp)
+    idr_02 = calculate_idr(mu[2], fsamp)
 
     # 2.1
     mu_num = 2
-    time = 0.025
-    sta_sig = sta.calculate_sta(emg=emg_signal, mu_sig=mu_pulses, mu_num=mu_num, time=time, fsamp=fsamp)
+    time = 0.02
+    sta_sig = calculate_sta(emg=emg, mu=mu, mu_num=mu_num, time=time, fsamp=fsamp)
 
+    # 3.1 Compute peak-to-peak values
+    p2p = calculate_p2p(emg=emg, mu=mu, time=time, fsamp=fsamp)
+    lax = test(emg, sorted_mu, time, fsamp)
+    k = 0
     # Plots
     # 1.1 Plot original spike train
     # plot_spike_trains_and_force(firing_matrix=firing_matrix, force_signal=force_signal, fsamp=fsamp)
@@ -202,7 +288,10 @@ def main():
     # plot_idr_and_force(idr_01=idr_01, idr_02=idr_02, mu_nr_01=1, mu_nr_02=2, force_signal=force_signal, fsamp=fsamp)
 
     # 2.1 Plot the STAs
-    plot_sta(sta_sig=sta_sig, mu_num=mu_num)
+    # plot_sta(sta_sig=sta_sig, mu_num=mu_num)
+
+    # 3.2 Plot the correlation (Can get only the p2p values, because this was calculated with the already sorted mus)
+    plot_max_p2p(p2p=lax)
 
 
 if __name__ == "__main__":
